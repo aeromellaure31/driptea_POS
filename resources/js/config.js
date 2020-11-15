f71128c66f3ca98761e4cdb9eacc2975407980b0
kind-soul07 <ziekind07.com>
2020-11-15 13:23:48 +0800

    pusher

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,6 +1,6 @@
 export default {
-    PUSHER_APP_ID: '1095899',
-    PUSHER_APP_KEY: '426e9e5cd2a694fa7c40',
-    PUSHER_APP_SECRET: '4f62e10c794a3d0f2161',
+    PUSHER_APP_ID: '1095901',
+    PUSHER_APP_KEY: 'ce14cbcfe36deee6fdd1',
+    PUSHER_APP_SECRET: '4f49ed6b0a9d10244f48',
     PUSHER_APP_CLUSTER: 'ap1',
 }
