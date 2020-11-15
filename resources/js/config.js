2f3cf16a8d9087f477e84276af53a078339e43e6
kind-soul07 <ziekind07.com>
2020-11-15 13:41:37 +0800

    change

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,6 +1,6 @@
 export default {
-    PUSHER_APP_ID: '1095901',
-    PUSHER_APP_KEY: 'ce14cbcfe36deee6fdd1',
-    PUSHER_APP_SECRET: '4f49ed6b0a9d10244f48',
-    PUSHER_APP_CLUSTER: 'ap1',
+    PUSHER_APP_ID: '1107421',
+    PUSHER_APP_KEY: 'd19e64ae570cf95d2d4b',
+    PUSHER_APP_SECRET: '76fc4fcd8f39f033d7f7',
+    PUSHER_APP_CLUSTER: 'us2',
 }
