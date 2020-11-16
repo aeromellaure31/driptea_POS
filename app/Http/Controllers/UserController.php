44083bcb3e3f8fd4d9ef756621aee1d0ac830fcd
kind-soul07 <ziekind07.com>
2020-11-16 08:46:02 +0800

    another change

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
             $filename = pathinfo($filenamewithextension, PATHINFO_FILENAME);
             $extension = $request->image->getClientOriginalExtension();
             $filenametostore = $filename.'_'.time().'.'.$extension;
-            Storage::disk('s3')->put($filenametostore, fopen($request->image, 'r+'), 'public');
+            Storage::disk('s3')->put($filenametostore, $request->image, 'public');
         // }
 
         $user = User::firstOrCreate(['id' => $request->id]);
