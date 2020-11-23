762105b31ca5b7fb7382d2b234fcae5854509607
kind-soul07 <ziekind07.com>
2020-11-23 12:57:19 +0800

    heroku push

--- resources/js/services/auth/index.js
+++ resources/js/services/auth/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import ROUTER from '../../router'
 export default {
     // url: 'http://ec2-34-229-81-230.compute-1.amazonaws.com/',
-    url: 'http://localhost:8000/',
+    // url: 'http://localhost:8000/',
+    url: 'https://driptea.herokuapp.com/',
     productPrice: null,
     imageSelected: null,
     token: null,
