b7da072ba2320e46adbfdeb9c0a8b0162b1b85b6
kind-soul07 <ziekind07.com>
2020-11-23 18:02:10 +0800

    new update

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>POS_Driptea</title>
 </head>
 <body>
