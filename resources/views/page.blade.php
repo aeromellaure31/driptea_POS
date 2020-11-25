db09892a232630409acf40697aa4a680845b993f
kind-soul07 <ziekind07.com>
2020-11-25 13:00:07 +0800

    new

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -8,7 +8,6 @@
     <link rel="icon" href="{{ URL::asset('favicon1.ico') }}" type="image/x-icon"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>POS_Driptea</title>
 </head>
 <body>
