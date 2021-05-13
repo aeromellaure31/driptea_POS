22ace28126a20c1144538175789d1d07bcac8f07
kind-soul07 <ziekind07.com>
2021-05-13 10:20:45 +0800

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -240,7 +240,6 @@ class OrderController extends Controller
             ->where('store_orders.deleted_at', null)
             ->where('store_orders.status', 'complete')
             ->groupBy('year','ProductName')
-            // ->orderBy('quan', 'desc')
             ->get();
         return response()->JSON(compact('prods'));
     }
